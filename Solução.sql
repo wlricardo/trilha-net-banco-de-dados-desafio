1397cab83cc44f8de479f3af7a8258cd3b7ef23d
Leonardo Buta <leonardo.lbuta@gmail.com>
2022-05-08 23:57:32 -0300

    Solução do desafio

---
+++ Solução.sql
@@ -0,0 +1,79 @@
+-- 1 : Buscar o nome e ano dos filmes
+SELECT 
+	Nome, Ano
+FROM Filmes;
+
+
+-- 2 : Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT 
+	Nome, Ano
+FROM Filmes
+ORDER BY Ano
+
+
+-- 3 : Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT 
+	Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta Para o Futuro';
+
+
+-- 4 : Buscar os filmes lançados em 1997
+SELECT * FROM Filmes
+WHERE Ano = 1997;
+
+
+-- 5 : Buscar os filmes lançados APÓS o ano 2000
+SELECT * FROM Filmes
+WHERE Ano > 2000;
+
+
+-- 6 : Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT * FROM Filmes
+WHERE (Duracao > 100 AND Duracao < 150)
+ORDER BY Duracao;
+
+
+-- 7 : Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+SELECT Ano, COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+
+-- 8 : Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT * FROM Atores
+WHERE Genero = 'M';
+
+
+-- 9 : Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT * FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+
+-- 10 : Buscar o nome do filme e o gênero
+SELECT 
+	Nome, Genero
+FROM 
+	Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON FilmesGenero.IdGenero = Generos.Id;
+
+
+-- 11 : Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT 
+	Nome, Genero
+FROM 
+	Filmes
+JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério';
+
+
+-- 12 : Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT
+	Nome, PrimeiroNome, UltimoNome, Papel
+FROM Filmes
+JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+JOIN Atores ON ElencoFilme.IdAtor = Atores.Id;
